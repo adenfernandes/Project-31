5f3ff1ddf86cbe6c49aa783934a7fb2e3ddfdd58
adenfernandes <73562846+adenfernandes@users.noreply.github.com>
2020-12-15 17:33:05 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var ground
+
+
+
+
+function setup() {
+  createCanvas(480,800);
+  
+
+function draw() {
+  background(255,255,255);  
+ 
+  ground=new Ground (800,665,1600,10)
+  ground.display();
+
+
+}
